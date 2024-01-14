986f9a9f402d74684fba3944127b22af2763a6e5
alejandro.gutierrez <alejandro.gutierrez.sge@mango.com>
2024-01-14 12:21:18 +0100

    initial commit with skeleton

---
+++ src/main/java/com/ecommerce/mspriceretriever/MsPriceRetrieverApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.mspriceretriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPriceRetrieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPriceRetrieverApplication.class, args);
+	}
+
+}
